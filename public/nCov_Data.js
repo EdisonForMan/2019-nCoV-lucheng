0e23fb6d56e2d4d8b6290518b73566a9d322b649
Edison_ForMan <chuanghewzh12@hotmail.com>
2020-02-02 11:12:25 +0800

    daily data

---
+++ public/nCov_Data.js
@@ -0,0 +1,53 @@
+/**
+ * daily update
+ * created by eds 2020/2/2
+ */
+const nCov_luchengData = {
+    /** 防疫布控  */
+    macroscopic: {
+        /** daily  */
+        death: 0,               //  死亡
+        cure: 1,                //  治愈
+        /** leftOptions  */
+        lc: {
+            qzbl: 41,           //  确诊病例
+            zzbl: 24,           //  疑似病例 
+            yydc: 26,
+            sqdc: 7,
+            gld: 18,
+            jjgl: 2416,
+            mj: 337,
+            hbhw: 1305
+        },
+        lc_yt: {
+            qzbl: 20,
+            zzbl: 5,
+            ytyg: 575,
+            gld: 18,
+            mj: 150
+        },
+        highway_type_1: 4,
+        highway_type_2: 1,
+        people_type_1: 110,
+        people_type_2: 91,
+        people_type_3: 10,
+        people_type_6: 93,
+        people_type_7: 28,
+        people_type_8: 24,
+        people_type_9: 41,
+        people_type_10: 14,
+        people_type_11: 4,
+        people_type_12: 251,
+        clinic_type_1: 8,
+        clinic_type_2: 1,
+        clinic_type_3: 122,
+        clinic_type_5: 8,
+        clinic_type_7: 1,
+        jzgd: 195,
+        nj_zdqy: 30,
+        nj_xm_1: 5,
+        nj_xm_2: 14,
+    },
+}
+
+window.nCov_luchengData = nCov_luchengData;
--- src/components/macroscopic/topDate.vue
+++ src/components/macroscopic/topDate.vue
@@ -2,19 +2,19 @@
   <div id="topDateDiv">
     <ul>
       <li>
-        <h3 style="color:#ff283a">39人</h3>
+        <h3 style="color:#ff283a">{{$window.nCov_luchengData.macroscopic.lc.qzbl}}人</h3>
         <p>确诊病例</p>
       </li>
       <li>
-        <h3 style="color:#fcc659">23人</h3>
+        <h3 style="color:#fcc659">{{$window.nCov_luchengData.macroscopic.lc.zzbl}}人</h3>
         <p>疑似病例</p>
       </li>
       <li>
-        <h3 style="color:#f6e31b">0人</h3>
+        <h3 style="color:#f6e31b">{{$window.nCov_luchengData.macroscopic.death}}人</h3>
         <p>死亡病例</p>
       </li>
       <li>
-        <h3 style="color:#1eff95">1人</h3>
+        <h3 style="color:#1eff95">{{$window.nCov_luchengData.macroscopic.cure}}人</h3>
         <p>治愈病例</p>
       </li>
     </ul>
