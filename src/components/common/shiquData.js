ba0e9a39b7d7151b7037dab54a4bf0bc1815895f
zjr <zheng_jiarui@163.com>
2020-02-07 18:16:21 +0800

    更新卫健委指标数据

--- src/components/common/shiquData.js
+++ src/components/common/shiquData.js
@@ -4,106 +4,64 @@ export const SHI_QU_DATA = {
 	zhishuData: {
 		// fblTitle: '单位(%)',
 		fblTitle: '',
-		fblDesc: '近15日确诊病例/常住人口 (截至2020年2月06日)',
+		fblDesc: '近15日确诊病例/常住人口 (截至2020年2月07日)',
 		fbl: [ // 二代及以上病例 近15日确诊病例/常住人口
-			{
-				value :'6.5',
-				key: '鹿城'
-			},
-			{
-				value :'5.5',
-				key: '龙湾'
-			},
-			{
-				value :'7.5',
-				key: '瓯海'
-			},
-			{
-				value :'8.5',
-				key: '洞头'
-			},
-			{
-				value :'3.5',
-				key: '乐清'
-			},
-			{
-				value :'5.0',
-				key: '瑞安'
-			},
-			{
-				value :'4.0',
-				key: '永嘉'
-			},
-			{
-				value :'4.5',
-				key: '文成'
-			},
-			{
-				value :'6.0 ',
-				key: '平阳'
-			},
-			{
-				value :'7.0',
-				key: '泰顺'
-			},
-			{
-				value :'9.0',
-				key: '苍南'
-			},
-			{
-				value :'8.0',
-				key: '龙港'
-			},
-			{
-				value :'9.5',
-				key: '浙南集聚区'
-			},
-			{
-				value :'10.0',
-				key: '瓯江口'
-			}		
+			{value: 7.5, key: "鹿城"},
+			{value: 5.5, key: "龙湾"},
+			{value: 6, key: "瓯海"},
+			{value: 6.5, key: "洞头"},
+			{value: 3.5, key: "乐清"},
+			{value: 5, key: "瑞安"},
+			{value: 4, key: "永嘉"},
+			{value: 4.5, key: "文成"},
+			{value: 7, key: "平阳"},
+			{value: 8, key: "泰顺"},
+			{value: 9, key: "苍南"},
+			{value: 8.5, key: "龙港"},
+			{value: 9.5, key: "浙南集聚区"},
+			{value: 10, key: "瓯江口"}		
 		],
 		// mzblsTitle: '单位(%)',
 		mzblsTitle: '',
-		mzblsDesc: '近15日全市非本地产生的二代以上病例占比 (截至2020年2月06日)',
+		mzblsDesc: '近15日全市非本地产生的二代以上病例占比 (截至2020年2月07日)',
 		mzbls: [ // 二代及以上病例 近15日全市非本地产生的二代以上病例占比
-			{value: 8.4, key: "鹿城"},
-			{value: 3.64, key: "龙湾"},
-			{value: 7.78, key: "瓯海"},
-			{value: 0, key: "洞头"},
-			{value: 6.09, key: "乐清"},
-			{value: 5.41, key: "瑞安"},
-			{value: 8.21, key: "永嘉"},
-			{value: 8.57, key: "文成"},
-			{value: 4.71, key: "平阳"},
+			{value: 8.52, key: "鹿城"},
+			{value: 3.33, key: "龙湾"},
+			{value: 8.18, key: "瓯海"},
+			{value: 5, key: "洞头"},
+			{value: 6.29, key: "乐清"},
+			{value: 5.75, key: "瑞安"},
+			{value: 8.28, key: "永嘉"},
+			{value: 8.75, key: "文成"},
+			{value: 5, key: "平阳"},
 			{value: 6, key: "泰顺"},
 			{value: 4, key: "苍南"},
 			{value: 0, key: "龙港"},
 			{value: 10, key: "浙南集聚区"},
-			{value: 6.28, key: "瓯江口"}
+			{value: 0, key: "瓯江口"}
 		],
 		// fxlTitle: '单位(%)',
 		fxlTitle: '',
-		fxlDesc: '近15日新增非自行前往发热门诊病例/近15日地区新增确定病例数 (截至2020年2月06日)',
+		fxlDesc: '近15日新增非自行前往发热门诊病例/近15日地区新增确定病例数 (截至2020年2月07日)',
 		fxl: [ // 主动发现病例 近15日新增非自行前往发热门诊病例/近15日地区新增确定病例数
-			{value: 9.6, key: "鹿城"},
-			{value: 25.45, key: "龙湾"},
-			{value: 24.44, key: "瓯海"},
+			{value: 10.37, key: "鹿城"},
+			{value: 30, key: "龙湾"},
+			{value: 27.27, key: "瓯海"},
 			{value: 0, key: "洞头"},
-			{value: 24.83, key: "乐清"},
-			{value: 22.7, key: "瑞安"},
-			{value: 22.86, key: "永嘉"},
-			{value: 34.29, key: "文成"},
-			{value: 18.82, key: "平阳"},
+			{value: 21.44, key: "乐清"},
+			{value: 24, key: "瑞安"},
+			{value: 23.45, key: "永嘉"},
+			{value: 35, key: "文成"},
+			{value: 20, key: "平阳"},
 			{value: 32, key: "泰顺"},
 			{value: 24, key: "苍南"},
 			{value: 24, key: "龙港"},
 			{value: 0, key: "浙南集聚区"},
-			{value: 40, key: "瓯江口"}
+			{value: 0, key: "瓯江口"}
 		],
 		// qzblsTitle: '单位(起)',
 		qzblsTitle: '',
-		qzblsDesc: '近15日新增聚集性疫情发生起数 (截至2020年2月06日)',
+		qzblsDesc: '近15日新增聚集性疫情发生起数 (截至2020年2月07日)',
 		qzbls: [ // 聚集性疫情 近15日新增聚集性疫情发生起数
 			{value: 4, key: "鹿城"},
 			{value: 6.5, key: "龙湾"},
@@ -122,7 +80,7 @@ export const SHI_QU_DATA = {
 		],
 		// jublsTitle: '单位(例)',
 		jublsTitle: '',
-		jublsDesc: '近15日新增聚集性疫情发生病例数 (截至2020年2月06日)',
+		jublsDesc: '近15日新增聚集性疫情发生病例数 (截至2020年2月07日)',
 		jubls: [ // 聚集性疫情 近15日新增聚集性疫情发生病例数
 			{value: 4, key: "鹿城"},
 			{value: 5.5, key: "龙湾"},
@@ -132,12 +90,12 @@ export const SHI_QU_DATA = {
 			{value: 3.5, key: "瑞安"},
 			{value: 6, key: "永嘉"},
 			{value: 7.5, key: "文成"},
-			{value: 5, key: "平阳"},
+			{value: 4.5, key: "平阳"},
 			{value: 6.5, key: "泰顺"},
 			{value: 8, key: "苍南"},
 			{value: 10, key: "龙港"},
 			{value: 10, key: "浙南集聚区"},
-			{value: 10, key: "瓯江口"}		
+			{value: 10, key: "瓯江口"}	
 		],
 	},
 	qushiData: {
