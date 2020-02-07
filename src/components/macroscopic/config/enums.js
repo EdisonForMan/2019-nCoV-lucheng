e73d3a699e0a5c908799d8c7dfc32a02d7d6602a
daoyi777 <752028254@qq.com>
2020-02-07 16:37:22 +0800

    数据修改

--- src/components/macroscopic/config/enums.js
+++ src/components/macroscopic/config/enums.js
@@ -13,8 +13,8 @@ export const leftOptions = [{
     children: [{
         name: `确诊病例 (${lc.qzbl}例)`,
         id: "qzbl",
-        url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/fangkong/MapServer",
-        sublayers: "1",
+        url: "http://172.20.89.7:6082/arcgis/rest/services/wz_yiqing/fangkong_wz/MapServer",
+        sublayers: "0",
         icon: "确诊病例",
         check: false
     }, {
@@ -35,103 +35,92 @@ export const leftOptions = [{
     }, {
         name: `疫点 (${lc.gld}个)`,
         id: "gld",
-        url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/paiban/MapServer",
-        sublayers: "3",
+        url: "http://172.20.89.7:6082/arcgis/rest/services/wz_yiqing/fangkong_wz/MapServer",
+        sublayers: "1",
         icon: "治愈",
         check: false
     }
     ]
 }, {
-    label: "医疗资源",
+    label: "网格管理",
     sflabel: -1,
     check: false,
     show: true,
     children: [{
-        name: `发热门诊 (${macroscopic.clinic_type_1}家)`,
-        id: "clinic_type_1",
-        url: "http://172.20.89.7:6082/arcgis/rest/services/weijian/resource/MapServer",
-        sublayers: "1",
-        icon: "发热门诊",
+        name: "街镇网格",
+        id: "chanyePlate",
+        sublayers: "0",
+        // isImg: true,
+        url: "http://172.20.89.7:6082/arcgis/rest/services/weijian/xzjd/MapServer",
+        check: true
+    }, {
+        name: "社区网格",
+        id: "wg",
+        sublayers: "12",
+        url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/fangkong/MapServer",
         check: false
     }, {
-        name: `定点救治医院 (${macroscopic.clinic_type_2}家)`,
-        id: "clinic_type_2",
-        url: "http://172.20.89.7:6082/arcgis/rest/services/weijian/resource/MapServer",
-        sublayers: "2",
-        icon: "定点医院",
+        name: "小区、大厦、物业",
+        id: "xq",
+        sublayers: "14",
+        // isImg: true,
+        url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/fangkong/MapServer",
         check: false
-    }, {
-        name: `其他专科医院 (${macroscopic.clinic_type_3}家)`,
-        id: "clinic_type_3",
-        url: "https://services.wzmap.gov.cn/server/rest/services/TDT/ZhuanTiSJ/MapServer",
-        sublayers: "8",
-        definitionExpression: "TAG LIKE '%专科医院%'",
-        icon: "其他专科医院",
-        check: false
-    },
-        {
-            name: "乡镇卫生医院 (7129家)",
-            id: "clinic_type_4",
-            url: "https://services.wzmap.gov.cn/server/rest/services/TDT/ZhuanTiSJ/MapServer",
-            sublayers: "8",
-            definitionExpression: "TAG LIKE '%医疗卫生%'",
-            icon: "乡镇卫生院",
-            check: false
-        },
-        {
-            name: `医保药房 (${macroscopic.clinic_type_5}家)`,
-            id: "clinic_type_5",
-            url: "https://services.wzmap.gov.cn/server/rest/services/TDT/ZhuanTiSJ/MapServer",
-            sublayers: "8",
-            definitionExpression: "TAG LIKE '%药房/保健品%'",
-            icon: "药店",
-            check: false
-        },
-        {
-            name: "其他医疗机构 (7129家)",
-            id: "clinic_type_6",
-            sublayers: "8",
-            url: "https://services.wzmap.gov.cn/server/rest/services/TDT/ZhuanTiSJ/MapServer",
-            icon: "其他医疗机构",
-            check: false
-        },
-        {
-            name: `疾控中心 (${macroscopic.clinic_type_7}家)`,
-            id: "clinic_type_7",
-            sublayers: "0",
-            url: "http://172.20.89.7:6082/arcgis/rest/services/weijian/resource/MapServer",
-            icon: "疾控中心",
-            check: false
-        }
-    ]
+    }]
 },
     {
-        label: "网格管理",
+        label: "卡口值班",
         sflabel: -1,
         check: false,
         show: true,
         children: [{
-            name: "街镇网格",
-            id: "chanyePlate",
+            name: `高速卡口 (${macroscopic.highway_type_1}个)`,
+            id: "highway_type_1",
+            url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/zhiban/MapServer",
             sublayers: "0",
-            // isImg: true,
-            url: "http://172.20.89.7:6082/arcgis/rest/services/weijian/xzjd/MapServer",
-            check: true
-        }, {
-            name: "小区、大厦、物业",
-            id: "xq",
-            sublayers: "12",
-            // isImg: true,
-            url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/fangkong/MapServer",
+            icon: "高速",
             check: false
-        }, {
-            name: "社区网格",
-            id: "wg",
-            sublayers: "14",
-            url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/fangkong/MapServer",
-            check: false
-        }]
-    }, {
+        },
+            // {
+            //     name: "汽车站 (4个)",
+            //     id: "highway_type_3",
+            //     url: "http://172.20.89.7:6082/arcgis/rest/services/weijian/crowded/MapServer",
+            //     sublayers: "0",
+            //     icon: "汽车站",
+            //     check: false
+            // },
+            {
+                name: `火车站 (${macroscopic.highway_type_2}个)`,
+                id: "highway_type_2",
+                url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/paiban/MapServer",
+                sublayers: "1",
+                icon: "火车站",
+                check: false
+            }, {
+                name: `商场超市 (${macroscopic.people_type_8}个)`,
+                id: "people_type_8",
+                url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/paiban/MapServer",
+                sublayers: "3",
+                icon: "商场超市",
+                check: false
+            }, {
+                name: `农贸市场 (${macroscopic.people_type_9}个)`,
+                id: "people_type_9",
+                url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/crowded/MapServer",
+                sublayers: "4",
+                icon: "农贸市场",
+                check: false
+            }, {
+                name: "小区进出口点",
+                id: "xqjck",
+                sublayers: "0",
+                icon: "进出口",
+                url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/crowded/MapServer",
+                check: false
+            }
+        ]
+    },
+    {
         label: "人员密集场所",
         sflabel: -1,
         check: false,
@@ -158,7 +147,7 @@ export const leftOptions = [{
             icon: "宗教",
             check: false
         }, {
-            name: `旅馆名单 (${macroscopic.people_type_3}个)`,
+            name: `专业市场 (${macroscopic.people_type_3}个)`,
             id: "people_type_3",
             url: "http://172.20.89.59/server/rest/services/lcjjdt/jjts/MapServer",
             sublayers: "3",
@@ -201,54 +190,94 @@ export const leftOptions = [{
                 sublayers: "8",
                 icon: "文化",
                 check: false
-            },
+            }
+        ]
+    },
+    {
+        label: "医疗资源",
+        sflabel: -1,
+        check: false,
+        show: true,
+        children: [{
+            name: `发热门诊 (${macroscopic.clinic_type_1}家)`,
+            id: "clinic_type_1",
+            url: "http://172.20.89.7:6082/arcgis/rest/services/weijian/resource/MapServer",
+            sublayers: "1",
+            icon: "发热门诊",
+            check: false
+        }, {
+            name: `定点救治医院 (${macroscopic.clinic_type_2}家)`,
+            id: "clinic_type_2",
+            url: "http://172.20.89.7:6082/arcgis/rest/services/weijian/resource/MapServer",
+            sublayers: "2",
+            icon: "定点医院",
+            check: false
+        }, {
+            name: `其他专科医院 (${macroscopic.clinic_type_3}家)`,
+            id: "clinic_type_3",
+            url: "https://services.wzmap.gov.cn/server/rest/services/TDT/ZhuanTiSJ/MapServer",
+            sublayers: "8",
+            definitionExpression: "TAG LIKE '%专科医院%'",
+            icon: "其他专科医院",
+            check: false
+        },
+            // {
+            //     name: "乡镇卫生医院 (7129家)",
+            //     id: "clinic_type_4",
+            //     url: "https://services.wzmap.gov.cn/server/rest/services/TDT/ZhuanTiSJ/MapServer",
+            //     sublayers: "8",
+            //     definitionExpression: "TAG LIKE '%医疗卫生%'",
+            //     icon: "乡镇卫生院",
+            //     check: false
+            // },
             {
-                name: `体育(游泳池) (${macroscopic.people_type_10}个)`,
-                id: "people_type_10",
-                url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/crowded/MapServer",
-                sublayers: "7",
-                icon: "健身房",
+                name: `医保药房 (${macroscopic.clinic_type_5}家)`,
+                id: "clinic_type_5",
+                url: "https://services.wzmap.gov.cn/server/rest/services/TDT/ZhuanTiSJ/MapServer",
+                sublayers: "8",
+                definitionExpression: "TAG LIKE '%药房/保健品%'",
+                icon: "药店",
                 check: false
             },
+            // {
+            //     name: "其他医疗机构 (7129家)",
+            //     id: "clinic_type_6",
+            //     sublayers: "8",
+            //     url: "https://services.wzmap.gov.cn/server/rest/services/TDT/ZhuanTiSJ/MapServer",
+            //     icon: "其他医疗机构",
+            //     check: false
+            // },
             {
-                name: `商场超市 (${macroscopic.people_type_10}个)`,
-                id: "people_type_10",
-                url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/crowded/MapServer",
-                sublayers: "7",
-                icon: "市场",
-                check: false
-            },
-            {
-                name: `农贸市场 (${macroscopic.people_type_10}个)`,
-                id: "people_type_10",
-                url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/crowded/MapServer",
-                sublayers: "7",
-                icon: "市场",
+                name: `疾控中心 (${macroscopic.clinic_type_7}家)`,
+                id: "clinic_type_7",
+                sublayers: "0",
+                url: "http://172.20.89.7:6082/arcgis/rest/services/weijian/resource/MapServer",
+                icon: "疾控中心",
                 check: false
             }
         ]
     },
     {
-        label: "三返专题",
-        sflabel: -1,
+        label: -1,
+        sflabel: "三返专题",
         check: false,
         show: true,
         children: [{
-            name: `企业 (${macroscopic.jzgd}家)`,
+            name: `建筑工地信息 (${macroscopic.jzgd}家)`,
             id: "jzgd",
             url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/fangkong/MapServer",
             sublayers: "13",
             icon: "建筑工地",
             check: false
         }, {
-            name: `学校 (${macroscopic.nj_zdqy}家)`,
+            name: `南郊重点企业 (${macroscopic.nj_zdqy}家)`,
             id: "nj_zdqy",
             url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/nanjiao/MapServer",
             sublayers: "4",
             icon: "重点企业",
             check: false
         }, {
-            name: `宗教 (${macroscopic.nj_xm_1}家)`,
+            name: `南郊街道自建项目企业 (${macroscopic.nj_xm_1}家)`,
             id: "nj_xm_1",
             url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/nanjiao/MapServer",
             sublayers: "1",
@@ -256,7 +285,7 @@ export const leftOptions = [{
             icon: "自建项目",
             check: false
         }, {
-            name: `事业单位 (${macroscopic.nj_xm_2}家)`,
+            name: `南郊街道在建项目企业 (${macroscopic.nj_xm_2}家)`,
             id: "nj_xm_2",
             url: "http://172.20.89.7:6082/arcgis/rest/services/lucheng/nanjiao/MapServer",
             sublayers: "1",
