800135654c3edcfc2933561a0f3c48aa47724e58
sherry1004 <775757661@qq.com>
2020-02-20 16:18:40 +0800

    详情框样式

--- src/components/monitor/frame/dkxqForm.vue
+++ src/components/monitor/frame/dkxqForm.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="dkxqForm">
     <a>×</a>
-
     <div class="block block1">
       <span class="title">基本详情</span>
       <div class="carousel">
@@ -12,9 +11,32 @@
     </div>
     <div class="block block2">
       <span class="title">区位优势</span>
+      <div>
+        <ul>
+          <li v-for="(item,index) in qktable" :key="index">
+            <p>{{item.title}}</p>
+            <span>{{item.text}}</span>
+          </li>
+        </ul>
+      </div>
     </div>
     <div class="block block3">
       <span class="title">问题清单</span>
+      <div>
+        <ul>
+          <li v-for="(item,index) in qtable" :key="index">
+            <div :style="{background:item.color}">
+              <span>{{item.text}}</span>
+              <br />
+              <span>{{item.tf}}</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="bottom">
+      <span>负责人：李先生</span>
+      <span>联系方式：0577-88030503</span>
     </div>
   </div>
 </template>
@@ -41,7 +63,32 @@ export default {
       now: "",
       time: "",
       charts: [],
-      sArr: []
+      sArr: [],
+      qktable: [
+        {
+          title: "项目区位优势",
+          text:
+            "本次规划项目位于温州书鹿城区，地理位置优越交通便利，翠微大道贯通南北；翠微山、黄龙山、景山三山环抱景观丰富；项目地块边界方整，周边商业、教育、医疗设备配套设施完善"
+        },
+        {
+          title: "项目区位优势",
+          text:
+            "本次规划项目位于温州书鹿城区，地理位置优越交通便利，翠微大道贯通南北；翠微山、黄龙山、景山三山环抱景观丰富；项目地块边界方整，周边商业、教育、医疗设备配套设施完善"
+        },
+        {
+          title: "项目区位优势",
+          text:
+            "本次规划项目位于温州书鹿城区，地理位置优越交通便利，翠微大道贯通南北；翠微山、黄龙山、景山三山环抱景观丰富；项目地块边界方整，周边商业、教育、医疗设备配套设施完善"
+        }
+      ],
+      qtable: [
+        { text: "审批到位", tf: "是", color: "rgba(58,209,75,0.3)" },
+        { text: "审批到位", tf: "是", color: "rgba(255,51,83,0.3)" },
+        { text: "审批到位", tf: "是", color: "rgba(58,209,75,0.3)" },
+        { text: "审批到位", tf: "是", color: "rgba(255,51,83,0.3)" },
+        { text: "审批到位", tf: "是", color: "rgba(255,51,83,0.3)" },
+        { text: "审批到位", tf: "是", color: "rgba(58,209,75,0.3)" }
+      ]
     };
   },
   components: {},
@@ -57,9 +104,10 @@ export default {
 #dkxqForm {
   position: absolute;
   width: 450px;
-  height: 720px;
-  background: #24386a;
-  border: 1px solid #04ecff;
+  height: 800px;
+  background: rgba(4, 30, 117, 0.5);
+  border-left: 1px solid #04ecff;
+  border-right: 1px solid #04ecff;
   border-radius: 6px;
   z-index: 20;
   top: 18%;
@@ -74,20 +122,42 @@ export default {
     font-size: 30px;
     cursor: pointer;
   }
-
+  span {
+    font-size: 14px;
+    color: #fff;
+  }
   .block {
     text-align: left;
+    width: 100%;
+    height: 250px;
+    background: rgba(4, 30, 117, 0.5);
+    border-bottom: 1px solid #4594ff;
+    margin-bottom: 8px;
+    border-top: 1px solid #4594ff;
     .title {
       font-size: 20px;
+      color: rgba(97, 235, 255, 1);
+      position: relative;
+      top: 5px;
+      left: 20px;
+      font-weight: 600;
+      border-left: 5px solid;
+      padding-left: 5px;
+    }
+    div {
+      width: 100%;
+      height: 90%;
+      box-sizing: border-box;
     }
   }
 
   .block1 {
-    height: 30%;
-    padding: 2% 0%;
-
+    // height: 30%;
+    // padding: 2% 0%;
     .carousel {
-      padding: 4px;
+      // padding: 4px;
+      height: 100%;
+      padding: 6px 20px;
 
       .el-carousel__item:nth-child(2n) {
         background-color: #99a9bf;
@@ -100,56 +170,74 @@ export default {
   }
 
   .block2 {
-    height: 30%;
-    padding: 2% 0%;
+    // height: 30%;
+    // padding: 2% 0%;
+    div {
+      padding: 10px;
+      ul {
+        width: 100%;
+        height: 100%;
+        overflow-y: auto;
+        background: rgba(31, 54, 92, 0.5);
+        li {
+          display: inline-block;
+          width: 100%;
+          height: 40px;
+          box-sizing: border-box;
+          padding: 19px;
+          p {
+            font-size: 16px;
+            color: rgba(211, 154, 69, 1);
+            line-height: 22px;
+          }
+        }
+      }
+    }
   }
 
   .block3 {
-    height: 30%;
-    padding: 2% 0%;
-  }
-
-  .choose_div {
-    margin: 2%;
-    padding: 2% 0%;
-
-    .wh_container {
-      position: absolute;
-      right: 0px;
-      z-index: 30;
-    }
-
-    span {
-      font-size: 16px;
-    }
-
-    img {
-      width: 16px;
-      height: 16px;
-      margin-left: 10px;
-      margin-right: 30px;
-      cursor: pointer;
-    }
-  }
-
-  .ctn2 {
-    height: 85%;
-    overflow: auto;
-
-    .chart {
-      width: 100%;
-      height: 230px;
-      margin: 10px 0px;
+    // height: 30%;
+    // padding: 2% 0%;
+    div {
+      padding: 10px;
+      ul {
+        width: 100%;
+        height: 100%;
+        width: 100%;
+        height: 100%;
+        list-style: none;
+        li {
+          width: 30%;
+          height: 50%;
+          display: inline-block;
+          text-align: center;
+          padding: 5px;
+          div {
+            width: 100%;
+            height: 56px;
+            display: inline-block;
+            padding-top: 10px;
+          }
+        }
+      }
     }
   }
+  .bottom {
+    width: 100%;
+    height: 30px;
+    line-height: 17px;
+    background: rgba(4, 30, 117, 0.5);
+    border-bottom: 1px solid #4594ff;
+    border-top: 1px solid #4594ff;
+  }
 
-  .content::-webkit-scrollbar {
+  .block2 ul::-webkit-scrollbar {
     display: block;
     width: 10px;
     background-color: rgb(107, 134, 223);
     border-radius: 3px;
   }
-  .content::-webkit-scrollbar-thumb {
+  .block2 ul::-webkit-scrollbar-thumb {
     width: 10px;
     background-color: rgb(14, 10, 247);
     border-radius: 3px;
